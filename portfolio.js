2fbc1f62825888725f8e756901680343a98febcb
dotSlashJack <5445jhes@gmail.com>
2019-12-26 21:22:24 -0800

    update links, begin portfolio

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function filter_content(selection) {
+    //alert(selection.options[selection.selectedIndex].text);
+    var x = document.getElementById("race_rev");
+    x.style.display = "none";
+    //undergrad_thesis, topicnet, race_rev
+    //TODO: make it shift by colunn not just row 
+    //(maybe we can make the outline here and set each individual inner html)
+}
